34ea6e3f148b3729bd02eb01d88feb8059326e77
David Gomez <davdg25@gmail.com>
2020-01-14 13:49:47 -0700

    Create coupon, merchant, order relationships and model testing

---
+++ db/migrate/20200114195658_create_coupons.rb
@@ -0,0 +1,9 @@
+class CreateCoupons < ActiveRecord::Migration[5.1]
+  def change
+    create_table :coupons do |t|
+      t.string :name
+      t.string :code
+      t.float :discount
+    end
+  end
+end
