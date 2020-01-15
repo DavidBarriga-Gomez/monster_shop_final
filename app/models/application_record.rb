5bc2b595848709dacbafee495ca67546a0cc5b99
David Gomez <davdg25@gmail.com>
2020-01-15 06:54:16 -0700

    delete application model

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,18 +1,18 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def initialize(contents)
-    @contents = contents
-  end
-
-  def coupon_total(merchant_id, discount)
-    @contents.sum do |item_id,quantity|
-      item = Item.find(item_id)
-      if item.merchant_id == merchant_id
-        (item.price * quantity) - (item.price * quantity) * (discount * 0.01)
-      else
-        item.price * quantity
-      end
-    end
-  end
+  # def initialize(contents)
+  #   @contents = contents
+  # end
+  #
+  # def coupon_total(merchant_id, discount)
+  #   @contents.sum do |item_id,quantity|
+  #     item = Item.find(item_id)
+  #     if item.merchant_id == merchant_id
+  #       (item.price * quantity) - (item.price * quantity) * (discount * 0.01)
+  #     else
+  #       item.price * quantity
+  #     end
+  #   end
+  # end
 end
